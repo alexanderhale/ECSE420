1bc8c66baf1583e843f0fc74e598f6ebabbc7a31
alexanderhale <a.hale@live.ca>
2019-11-24 10:21:33 -0500

    project WIP C and Python files

---
+++ ECSE420-Project/kernel (1).cu
@@ -0,0 +1,174 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// Defined min and max functions 
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+
+__global__ void mergeSort(int* startingArr, int* endingArr, unsigned int N, unsigned int level, unsigned int threads) {
+	
+	// Assigns thread ID 
+	unsigned int id = threadIdx.x + blockIdx.x * blockDim.x;
+	
+	// Creates individual portions to be sorted based off level, adds 1 if a remainder exists 
+	unsigned int totalArrays = N/(level);
+	if (totalArrays * level != N) {
+		totalArrays = totalArrays + 1;
+	}
+
+	// Initial start is based off of ID and current level 
+	unsigned arrayBeginning = id * level;
+	
+	// For loop for mergesort, increments by number of threads over time 
+	for (unsigned int currentArray = id; currentArray < totalArrays; currentArray=currentArray+threads) {
+		
+		// Takes middle and end values for single sort
+		unsigned arrayMiddle = MIN(arrayBeginning + (level / 2), N);
+		unsigned arrayEnd = MIN(arrayBeginning + level, N);
+		
+		// Creates indexes for individual sort to increment
+		unsigned int leftArrayIndex = arrayBeginning;
+		unsigned int rightArrayIndex = arrayMiddle;
+		
+		// Individual merge for given "level" worth of values 
+		for (unsigned int i = arrayBeginning; i < arrayEnd; i++) {
+			
+			// If middle array is larger, temp start is index in sorted array 
+			if (leftArrayIndex < arrayMiddle && (rightArrayIndex >= arrayEnd || startingArr[leftArrayIndex] < startingArr[rightArrayIndex])) {
+				endingArr[i] = startingArr[leftArrayIndex];
+				leftArrayIndex++;
+			}
+
+			// If middle array start is smaller, middle array is index in sorted array 
+			else {
+			
+				endingArr[i] = startingArr[rightArrayIndex];
+				rightArrayIndex++;
+			}
+		}
+		
+		// Increments to next start point based off of thread count 
+		arrayBeginning = arrayBeginning + threads*level; 
+		
+	}
+	
+}
+
+int main() {
+
+	// Define
+	printf("Paralyzed Merge Sort Has Begun");
+
+	unsigned int N = 1000000;
+	unsigned int threads = 1024;
+
+	// Define arrays for system
+	int* startingArray = new int[N];
+	int* endingArray = new int[N];
+	int* cudaStartArray;
+	int* cudaEndArray;
+
+	// Initialize starting array to values of 0 to 10000 randomly   
+	for (int i = 0; i < N; i++) {
+		startingArray[i] = rand()%10000;
+	}
+
+	// Copy starting Array values into end Array
+	
+	memcpy(endingArray, startingArray, N * sizeof(int));
+	
+	// Clock values 
+
+	clock_t clockStart, clockEnd;
+	clockStart = clock();
+
+	for (int level = 2; level < N*2 ; level=level*2) {
+				 
+		cudaSetDevice(0);
+		
+		// Allocate cuda memory for start and end array 
+
+		cudaMalloc((void**)& cudaStartArray, N *  sizeof(int));
+		cudaMalloc((void**)& cudaEndArray, N * sizeof(int));
+
+		// Copy data from original arrays into cuda version 
+
+		cudaMemcpy(cudaStartArray, startingArray, N * sizeof(int), cudaMemcpyHostToDevice);
+		cudaMemcpy(cudaEndArray, endingArray, N * sizeof(int), cudaMemcpyHostToDevice);
+
+		// Change thread count based off of input 
+
+		if (threads > 1024) {
+			threads = 1024;
+		}
+
+		// Merge sort formula 
+
+		mergeSort << <1, threads >> >  (cudaStartArray, cudaEndArray, N, level, threads);
+		
+		// Synchronize threads 
+
+		cudaDeviceSynchronize();
+
+		// Copy end array outcome from cuda version to original
+
+		cudaMemcpy(endingArray, cudaEndArray, N * sizeof(int), cudaMemcpyDeviceToHost);
+
+		// Free cuda memory 
+
+		cudaFree(cudaStartArray);
+		cudaFree(cudaEndArray);
+
+		printf("\nAt Iteration : %f Max Sections Sorted : %d", log2(level), level);	
+		
+		/*
+		//Prints individual value at each index 
+		for (int j = 0; j < N; j++) {
+			printf("%d ", endingArray[j]);
+			printf("\n");
+		}
+		*/
+
+		// Copies endingArray to startingArray  
+		memcpy(startingArray, endingArray, N*sizeof(int));
+
+	}
+
+	// End time clock value  
+	clockEnd = clock();
+	
+	printf(" \nFINAL ITERATION : \n");
+	
+	// Checks to see if merge sorted matrix is properly sorted 
+	bool isTrue = true; 
+	for (unsigned int i = 0; i < N; i++) {
+		//printf("%d \n", endingArray[j]);
+		if (i > 0) {
+			if (endingArray[i] < endingArray[i - 1]) {
+				isTrue = false;
+			}
+		}
+	}
+
+	// Prints if matrix has or hasn't been sorted 
+	if (isTrue) {
+		printf("The Input Array Is Sorted From Merge Sort \n");
+	}
+	else {
+		printf("The Input Array Is Not Sorted From Merge Sort \n");
+	}
+
+	// Prints total time of system 
+	printf("Total Merge Sort Time :  %f", ((float)(clockEnd - clockStart)) / CLOCKS_PER_SEC);
+	
+	return 0;
+}
+
+
---
+++ ECSE420-Project/mergeSort_Logic_Skeleton.cpp
@@ -0,0 +1,123 @@
+/**
+ * mergesort.cu
+ * a one-file c++ / cuda program for performing mergesort on the GPU
+ * While the program execution is fairly slow, most of its runnning time
+ *  is spent allocating memory on the GPU.
+ * For a more complex program that performs many calculations,
+ *  running on the GPU may provide a significant boost in performance
+ */
+
+#define min(a, b) (a < b ? a : b)
+
+int main() {
+
+    dim3 threadsPerBlock;
+    dim3 blocksPerGrid;
+
+    threadsPerBlock.x = 32;
+    threadsPerBlock.y = 1;
+    threadsPerBlock.z = 1;
+
+    blocksPerGrid.x = 8;
+    blocksPerGrid.y = 1;
+    blocksPerGrid.z = 1;
+
+    long* data; // some array to be sorted
+
+    //
+    // Allocate two arrays on the GPU
+    // we switch back and forth between them during the sort
+    //
+    long* D_data;
+    long* D_swp;
+    dim3* D_threads;
+    dim3* D_blocks;
+
+    // Copy from our input list into the first array
+    cudaMemcpy(D_data, data, size * sizeof(long), cudaMemcpyHostToDevice);
+ 
+    //
+    // Copy the thread / block info to the GPU
+    //
+    cudaMalloc((void**) &D_threads, sizeof(dim3));
+    cudaMalloc((void**) &D_blocks, sizeof(dim3));
+
+    cudaMemcpy(D_threads, &threadsPerBlock, sizeof(dim3), cudaMemcpyHostToDevice);
+    cudaMemcpy(D_blocks, &blocksPerGrid, sizeof(dim3), cudaMemcpyHostToDevice);
+
+    long* A = D_data;
+    long* B = D_swp;
+
+    long nThreads = threadsPerBlock.x * threadsPerBlock.y * threadsPerBlock.z *
+                    blocksPerGrid.x * blocksPerGrid.y * blocksPerGrid.z;
+
+    //
+    // Slice up the list and give pieces of it to each thread, letting the pieces grow
+    // bigger and bigger until the whole list is sorted
+    //
+    for (int width = 2; width < (size << 1); width <<= 1) {
+        long slices = size / ((nThreads) * width) + 1;
+
+        // Actually call the kernel
+        gpu_mergesort<<<blocksPerGrid, threadsPerBlock>>>(A, B, size, width, slices, D_threads, D_blocks);
+
+        // Switch the input / output arrays instead of copying them around
+        A = A == D_data ? D_swp : D_data;
+        B = B == D_data ? D_swp : D_data;
+    }
+
+    //
+    // Get the list back from the GPU
+    //
+    cudaMemcpy(data, A, size * sizeof(long), cudaMemcpyDeviceToHost);
+}
+
+// GPU helper function
+// calculate the id of the current thread
+__device__ unsigned int getIdx(dim3* threads, dim3* blocks) {
+    int x;
+    return threadIdx.x +
+           threadIdx.y * (x  = threads->x) +
+           threadIdx.z * (x *= threads->y) +
+           blockIdx.x  * (x *= threads->z) +
+           blockIdx.y  * (x *= blocks->z) +
+           blockIdx.z  * (x *= blocks->y);
+}
+
+//
+// Perform a full mergesort on our section of the data.
+//
+__global__ void gpu_mergesort(long* source, long* dest, long size, long width, long slices, dim3* threads, dim3* blocks) {
+    unsigned int idx = getIdx(threads, blocks);
+    long start = width*idx*slices, 
+         middle, 
+         end;
+
+    for (long slice = 0; slice < slices; slice++) {
+        if (start >= size)
+            break;
+
+        middle = min(start + (width >> 1), size);
+        end = min(start + width, size);
+        gpu_bottomUpMerge(source, dest, start, middle, end);
+        start += width;
+    }
+}
+
+//
+// Finally, sort something
+// gets called by gpu_mergesort() for each slice
+//
+__device__ void gpu_bottomUpMerge(long* source, long* dest, long start, long middle, long end) {
+    long i = start;
+    long j = middle;
+    for (long k = start; k < end; k++) {
+        if (i < middle && (j >= end || source[i] < source[j])) {
+            dest[k] = source[i];
+            i++;
+        } else {
+            dest[k] = source[j];
+            j++;
+        }
+    }
+}
